1bf52237c62a209098e9faaa9ee4a4ef8bcd4524
jjgilces <jjgilces@espol.edu.ec>
2025-03-06 16:53:54 -0600

    reusable service call

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//we can add the header for the bearer token here
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// GET 
+export const getData = async <T>(endpoint: string): Promise<T> => {
+  const response = await axiosInstance.get<T>(endpoint);
+  return response.data;
+};
+
+// POST 
+export const postData = async <T>(endpoint: string, payload: unknown): Promise<T> => {
+  const response = await axiosInstance.post<T>(endpoint, payload);
+  return response.data;
+};
+
+// PUT 
+export const putData = async <T>(endpoint: string, payload: unknown): Promise<T> => {
+  const response = await axiosInstance.put<T>(endpoint, payload);
+  return response.data;
+};
+
+// DELETE 
+export const deleteData = async <T>(endpoint: string): Promise<T> => {
+  const response = await axiosInstance.delete<T>(endpoint);
+  return response.data;
+};
+
+// DELETE 
+export const patchData = async <T>(endpoint: string): Promise<T> => {
+    const response = await axiosInstance.patch<T>(endpoint);
+    return response.data;
+  };
