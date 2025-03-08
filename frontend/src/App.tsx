ef6057e1d67140c014b994f63734db37a288a945
Ayomide Omotosho <aomotosho4@gmail.com>
2025-03-08 14:45:04 -0600

    chore: remove unused css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import UserProfile from './components/UserProfile';
 import './App.css'
 import FormDemo from './pages/FormDemo';
+import ManageGuests from './pages/ManageGuests';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/users/:id" element={<UserProfile />} />
         <Route path="/formdemo" element={<FormDemo />} />
+        <Route path="/guests" element={<ManageGuests />} />
       </Routes>
     </BrowserRouter>
   )
