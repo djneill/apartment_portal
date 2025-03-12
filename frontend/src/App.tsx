cec38ade65a0bfe86d6fd721b6af217f882a5721
jjgilces <jjgilces@espol.edu.ec>
2025-03-11 19:41:25 -0500

    navbar mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,20 @@ import UserProfile from './components/UserProfile';
 import './App.css'
 import FormDemo from './pages/FormDemo';
 import ReportIssue from './pages/guest/ReportIssue';
+import Layout from './components/Layout'; 
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/reportissue" element={<ReportIssue />} />
-        <Route path="/users/:id" element={<UserProfile />} />
-        <Route path="/formdemo" element={<FormDemo />} />
+      <Route path="/" element={<Login />} />
+          <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/reportissue" element={<ReportIssue />} />
+          <Route path="/users/:id" element={<UserProfile />} />
+          <Route path="/formdemo" element={<FormDemo />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
