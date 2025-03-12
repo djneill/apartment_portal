de5f93d206ceaff9462fe84310c208d29843f394
David Ogden <david-ogden699@hotmail.com>
2025-03-12 11:50:52 -0500

    added Id property due to new PK on issue table

--- apartment_portal_api/apartment_portal_api/Models/Issues/IssueResponse.cs
+++ apartment_portal_api/apartment_portal_api/Models/Issues/IssueResponse.cs
@@ -1,5 +1,4 @@
-﻿using apartment_portal_api.DTOs;
-using apartment_portal_api.Models.IssueTypes;
+﻿using apartment_portal_api.Models.IssueTypes;
 using apartment_portal_api.Models.Statuses;
 using apartment_portal_api.Models.Users;
 
@@ -7,6 +6,7 @@ namespace apartment_portal_api.Models.Issues;
 
 public class IssueResponse
 {
+    public int Id { get; set; }
     public string Description { get; set; }
     public DateTime CreatedOn { get; set; }
     public IssueTypeResponse IssueType { get; set; }
