cbc9a260adccf502903f9d7c4e5896db78ccc5a6
David Ogden <david-ogden699@hotmail.com>
2025-03-15 00:45:53 -0500

    removed email column from guests table

--- apartment_portal_api/apartment_portal_api/DTOs/GuestPatchDTO.cs
+++ apartment_portal_api/apartment_portal_api/DTOs/GuestPatchDTO.cs
@@ -4,7 +4,6 @@ public class GuestPatchDTO
         public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? AccessCode { get; set; }
         public DateTime? Expiration { get; set; }
---
+++ apartment_portal_api/apartment_portal_api/Data/Migrations/20250315053557_removeEmailFromGuests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apartment_portal_api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeEmailFromGuests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "guests_email_key",
+                table: "guests");
+
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "guests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "guests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "guests_email_key",
+                table: "guests",
+                column: "email",
+                unique: true);
+        }
+    }
+}
