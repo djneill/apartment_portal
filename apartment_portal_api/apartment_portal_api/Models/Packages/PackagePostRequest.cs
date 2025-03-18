9022b7fa10ad82301573c0f7851525b874de4e99
David Ogden <david-ogden699@hotmail.com>
2025-03-18 11:03:40 -0500

    route fixes and code cleanup

--- apartment_portal_api/apartment_portal_api/Models/Packages/PackagePostRequest.cs
+++ apartment_portal_api/apartment_portal_api/Models/Packages/PackagePostRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace apartment_portal_api.Models.Packages;
 
-public record PackagePostRequest(int UnitUsersId, int LockerNumber, int Code, int StatusId);
+public record PackagePostRequest(int UnitId, int LockerNumber, int StatusId);
