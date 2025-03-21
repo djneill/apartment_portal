42bd4e3fc76557610363d28a241d89b08135b99e
David Ogden <david-ogden699@hotmail.com>
2025-03-21 12:14:08 -0500

    refactored notifications route

--- apartment_portal_api/apartment_portal_api/Controllers/NotificationController.cs
+++ apartment_portal_api/apartment_portal_api/Controllers/NotificationController.cs
@@ -1,116 +1,195 @@
+using System.Linq.Expressions;
 using apartment_portal_api.Abstractions;
 using apartment_portal_api.Models.Issues;
 using apartment_portal_api.Models.Notifications;
 using apartment_portal_api.Models.Packages;
-using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using apartment_portal_api.Models.LeaseAgreements;
+using apartment_portal_api.Models.UnitUsers;
 
 namespace apartment_portal_api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class NotificationController : ControllerBase
+public class NotificationController(IUnitOfWork unitOfWork) : ControllerBase
 {
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly IMapper _mapper;
-
-    public NotificationController(IUnitOfWork unitOfWork, IMapper mapper)
-    {
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
-    }
-
     [HttpGet("latest")]
-    public async Task<ActionResult<ICollection<NotificationDTO>>> GetLatestNotifications([FromQuery] int? userId, [FromQuery] int? limit)
+    public async Task<ActionResult<ICollection<NotificationDTO>>> GetLatestNotifications([FromQuery] int userId = 0)
     {
-        //var loggedInUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-        //if(loggedInUserId == null)
-        //{
-        //    return Unauthorized();
-        //}
-
-        //int.TryParse(loggedInUserId, out int loggedInUserIdInt);
-        //var isAdmin = User.IsInRole("Admin");
-
-        //if (!isAdmin && userId.HasValue && userId != loggedInUserIdInt)
-        //{
-        //    return Forbid();
-        //}
+        var isUserOrAdmin = IsUserOrAdmin(userId);
+        if (isUserOrAdmin is not null) return isUserOrAdmin;
 
 
-        var userUnits = await _unitOfWork.UnitUserRepository.GetAsync(uu =>
-        //(isAdmin || uu.UserId == loggedInUserIdInt) &&
-        (!userId.HasValue || uu.UserId == userId)
-        );
-        var userUnitIds = userUnits.Select(uu => uu.UnitId).ToList();
-        if (!userUnitIds.Any()) return NotFound("User is not associated with any units.");
+        UnitUser[] unitUsers = await GetUnitUsers(userId);
+        if (unitUsers.Length == 0) return BadRequest("User is not associated with any units.");
 
-        // int userUnitId = userUnit.First().UnitId;
+        var userUnitIds = unitUsers.Select(uu => uu.UnitId);
 
-        var packageLists = await _unitOfWork.PackageRepository.GetAsync(p =>
-        (//isAdmin || 
-         userUnitIds.Contains(p.UnitId)) &&
-        (!userId.HasValue || (userUnitIds.Contains(p.UnitId) && p.Status.Name == "Arrived")),
-            includeProperties: nameof(Package.Status)
-        );
-        var packageList = packageLists.ToList();
+        List<NotificationDTO> packages = await GetPackageNotifications(userUnitIds, userId);
 
-        var packages = packageList.Select(p => new NotificationDTO
-        {
-            Type = "Package",
-            Message = $"You have a new package at the front desk. Status: {(p.Status != null ? p.Status.Name : "Unknown")}",
-            Date = DateTime.UtcNow
-        })
+        List<NotificationDTO> issues = await GetIssueNotifications(userId);
+        
+        List<NotificationDTO> leaseNotifications = await GetLeaseAgreementNotifications(userId);
+
+        var notifications = leaseNotifications
+            .Concat(packages)
+            .Concat(issues)
+            .OrderBy(n => n.Date)
             .ToList();
 
+        return Ok(notifications);
+    }
 
-        var issueLists = await _unitOfWork.IssueRepository.GetAsync(i =>
-        // (isAdmin || i.UserId == loggedInUserIdInt) &&
-        (!userId.HasValue || (i.UserId == userId && i.Status.Name != "Inactive")),
-        includeProperties: nameof(Issue.Status)
-        );
 
-        var issueList = issueLists.ToList();
+    private async Task<List<NotificationDTO>> GetLeaseAgreementNotifications(int userId)
+    {
+        List<NotificationDTO> leaseNotifications = [];
+        IEnumerable<LeaseAgreement> leasesResponse;
+
+        if (userId > 0)
+        {
+            leasesResponse = await unitOfWork.LeaseAgreementRepository.GetAsync(lease =>
+                    lease.LeaseStatusId == 3 &&
+                    lease.UnitUser.UserId == userId ||
+                    lease.LeaseStatusId == 1 &&
+                    lease.UnitUser.UserId == userId,
+                nameof(LeaseAgreement.UnitUser));
+        }
+        else
+        {
+            leasesResponse = await unitOfWork.LeaseAgreementRepository.GetAsync(lease =>
+                    lease.LeaseStatusId == 3 ||
+                    lease.LeaseStatusId == 1,
+                $"{nameof(LeaseAgreement.UnitUser)}");
+        }
+
+        foreach (var lease in leasesResponse)
+        {
+            if (lease.LeaseStatusId == 3)
+            {
+                DateTime startDateTime = lease.StartDate.ToDateTime(new TimeOnly());
+
+                NotificationDTO notification = new()
+                {
+                    Type = "Lease",
+                    Date = startDateTime,
+                    Message = $"Reminder: Your upcoming lease needs to be signed before {startDateTime:MMMM dd, yyyy}."
+                };
+
+                if (userId == 0)
+                    notification.Message =
+                        $"Unit {lease.UnitUser.Unit.Number}: Lease signature is due by {startDateTime:MMMM dd, yyyy}.";
+
+                leaseNotifications.Add(notification);
+
+                continue;
+            }
+
+
+            
+            DateTime expiration = lease.EndDate.ToDateTime(new TimeOnly());
+            int daysUntilExpiration = (int)(expiration - DateTime.UtcNow).TotalDays;
+            if (lease.LeaseStatusId == 1 && daysUntilExpiration <= 30 && daysUntilExpiration >= 0)
+            {
+                var notification = new NotificationDTO
+                {
+                    Type = "Lease",
+                    Message = $"Reminder: Your lease expires on {expiration:MMMM dd, yyyy}.",
+                    Date = expiration
+                };
+
+                if (userId == 0) notification.Message =
+                    $"Unit {lease.UnitUser.Unit.Number}: Lease expires on {expiration:MMMM dd, yyyy}.";
+
+                leaseNotifications.Add(notification);
+            }
+        }
+
+        return leaseNotifications;
+    }
+
+    private async Task<List<NotificationDTO>> GetIssueNotifications(int userId)
+    {
+        IEnumerable<Issue> issueList;
+        if (userId > 0)
+        {
+            issueList = await unitOfWork.IssueRepository.GetAsync(i =>
+                    i.UserId == userId && i.Status.Name != "Inactive",
+                includeProperties: nameof(Issue.Status)
+            );
+        }
+        else
+        {
+            issueList = await unitOfWork.IssueRepository.GetAsync(i => i.Status.Name != "Inactive", nameof(Issue.Status));
+        }
+        
         var issues = issueList
             .OrderByDescending(i => i.CreatedOn)
             .Select(i => new NotificationDTO
             {
                 Type = "Issue",
-                Message = $"Issue: {i.Description}. Status: {(i.Status != null ? i.Status.Name : "Unknown")}",
+                Message = $"Issue: {i.Description}. Status: {i.Status.Name}",
                 Date = i.CreatedOn
             })
             .ToList();
 
-        var users = await _unitOfWork.UserRepository.GetAsync(u =>
-        //(isAdmin || u.Id == loggedInUserIdInt) && 
-        (!userId.HasValue || u.Id == userId)
-        );
-        var user = users.FirstOrDefault();
+        return issues;
+    }
 
-        var leaseNotification = new List<NotificationDTO>();
-        if (user != null)
+    private async Task<List<NotificationDTO>> GetPackageNotifications(IEnumerable<int> userUnitIds, int userId)
+    {
+        IEnumerable<Package> packageLists = [];
+        if (userId > 0)
         {
-            DateTime leaseExpiration = user.CreatedOn.AddYears(1);
-            var daysUntilExpiration = (leaseExpiration - DateTime.UtcNow).TotalDays;
-            if (daysUntilExpiration <= 30)
-            {
-                leaseNotification.Add(new NotificationDTO
-                {
-                    Type = "Lease",
-                    Message = $"Reminder: Your lease expires on {leaseExpiration:MMMM dd, yyyy}.",
-                    Date = leaseExpiration
-                });
-            }
+            packageLists = await unitOfWork.PackageRepository.GetAsync(p =>
+                    userUnitIds.Contains(p.UnitId) && p.Status.Name == "Arrived",
+                nameof(Package.Status));
         }
 
-        var notifications = packages
-            .Concat(issues)
-            .Concat(leaseNotification)
-            .OrderByDescending(n => n.Date)
-            .Take(limit.GetValueOrDefault(10))
+        var packages = packageLists.Select(p => new NotificationDTO
+            {
+                Type = "Package",
+                Message = $"You have a new package at the front desk. Status: {p.Status.Name}",
+                Date = DateTime.UtcNow
+            })
             .ToList();
 
-        return Ok(notifications);
+        return packages;
+    }
+
+    private async Task<UnitUser[]> GetUnitUsers(int userId)
+    {
+        IEnumerable<UnitUser> unitUsersRes;
+
+        if (userId > 0)
+        {
+            unitUsersRes = await unitOfWork.UnitUserRepository.GetAsync(uu => uu.UserId == userId, nameof(UnitUser.Unit));
+        }
+        else
+        {
+            unitUsersRes = await unitOfWork.UnitUserRepository.GetAsync(includeProperties: nameof(UnitUser.Unit));
+        }
+
+        var unitUsers = unitUsersRes.ToArray();
+
+        return unitUsers;
+    }
+    private ActionResult? IsUserOrAdmin(int requestUserId)
+    {
+        var userClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+        if (userClaim is null)
+        {
+            return Unauthorized();
+        }
+
+        bool isAdmin = User.IsInRole("Admin");
+
+        if (!isAdmin && userClaim.Value != requestUserId.ToString())
+        {
+            return Forbid();
+        }
+
+        return null;
     }
 }
