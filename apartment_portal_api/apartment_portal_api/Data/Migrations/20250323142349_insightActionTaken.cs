51c12800d0543c1f2693d8915def6ecb76f56c58
David Ogden <david-ogden699@hotmail.com>
2025-03-23 10:28:56 -0500

    added actionTaken column to insights and a Patch request to update

---
+++ apartment_portal_api/apartment_portal_api/Data/Migrations/20250323142349_insightActionTaken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apartment_portal_api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class insightActionTaken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "actionTaken",
+                table: "insights",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "actionTaken",
+                table: "insights");
+        }
+    }
+}
