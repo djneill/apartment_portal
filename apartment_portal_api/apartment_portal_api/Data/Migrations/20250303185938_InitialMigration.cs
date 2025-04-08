ffeebd0041998965a37164dabfd19dfe32cd3b47
David Ogden <david-ogden699@hotmail.com>
2025-04-08 09:41:53 -0500

    fixed migrations for easy initialization

--- apartment_portal_api/apartment_portal_api/Data/Migrations/20250303185938_InitialMigration.cs
+++ apartment_portal_api/apartment_portal_api/Data/Migrations/20250303185938_InitialMigration.cs
@@ -11,23 +11,23 @@ public partial class InitialMigration : Migration
     /// <inheritdoc />
     protected override void Up(MigrationBuilder migrationBuilder)
     {
-        migrationBuilder.AlterDatabase()
-            .Annotation("Npgsql:Enum:auth.aal_level", "aal1,aal2,aal3")
-            .Annotation("Npgsql:Enum:auth.code_challenge_method", "s256,plain")
-            .Annotation("Npgsql:Enum:auth.factor_status", "unverified,verified")
-            .Annotation("Npgsql:Enum:auth.factor_type", "totp,webauthn,phone")
-            .Annotation("Npgsql:Enum:auth.one_time_token_type", "confirmation_token,reauthentication_token,recovery_token,email_change_token_new,email_change_token_current,phone_change_token")
-            .Annotation("Npgsql:Enum:pgsodium.key_status", "default,valid,invalid,expired")
-            .Annotation("Npgsql:Enum:pgsodium.key_type", "aead-ietf,aead-det,hmacsha512,hmacsha256,auth,shorthash,generichash,kdf,secretbox,secretstream,stream_xchacha20")
-            .Annotation("Npgsql:Enum:realtime.action", "INSERT,UPDATE,DELETE,TRUNCATE,ERROR")
-            .Annotation("Npgsql:Enum:realtime.equality_op", "eq,neq,lt,lte,gt,gte,in")
-            .Annotation("Npgsql:PostgresExtension:extensions.pg_stat_statements", ",,")
-            .Annotation("Npgsql:PostgresExtension:extensions.pgcrypto", ",,")
-            .Annotation("Npgsql:PostgresExtension:extensions.pgjwt", ",,")
-            .Annotation("Npgsql:PostgresExtension:extensions.uuid-ossp", ",,")
-            .Annotation("Npgsql:PostgresExtension:graphql.pg_graphql", ",,")
-            .Annotation("Npgsql:PostgresExtension:pgsodium.pgsodium", ",,")
-            .Annotation("Npgsql:PostgresExtension:vault.supabase_vault", ",,");
+        //migrationBuilder.AlterDatabase()
+        //    .Annotation("Npgsql:Enum:auth.aal_level", "aal1,aal2,aal3")
+        //    .Annotation("Npgsql:Enum:auth.code_challenge_method", "s256,plain")
+        //    .Annotation("Npgsql:Enum:auth.factor_status", "unverified,verified")
+        //    .Annotation("Npgsql:Enum:auth.factor_type", "totp,webauthn,phone")
+        //    .Annotation("Npgsql:Enum:auth.one_time_token_type", "confirmation_token,reauthentication_token,recovery_token,email_change_token_new,email_change_token_current,phone_change_token")
+        //    .Annotation("Npgsql:Enum:pgsodium.key_status", "default,valid,invalid,expired")
+        //    .Annotation("Npgsql:Enum:pgsodium.key_type", "aead-ietf,aead-det,hmacsha512,hmacsha256,auth,shorthash,generichash,kdf,secretbox,secretstream,stream_xchacha20")
+        //    .Annotation("Npgsql:Enum:realtime.action", "INSERT,UPDATE,DELETE,TRUNCATE,ERROR")
+        //    .Annotation("Npgsql:Enum:realtime.equality_op", "eq,neq,lt,lte,gt,gte,in")
+        //    .Annotation("Npgsql:PostgresExtension:extensions.pg_stat_statements", ",,")
+        //    .Annotation("Npgsql:PostgresExtension:extensions.pgcrypto", ",,")
+        //    .Annotation("Npgsql:PostgresExtension:extensions.pgjwt", ",,")
+        //    .Annotation("Npgsql:PostgresExtension:extensions.uuid-ossp", ",,")
+        //    .Annotation("Npgsql:PostgresExtension:graphql.pg_graphql", ",,")
+        //    .Annotation("Npgsql:PostgresExtension:pgsodium.pgsodium", ",,")
+        //    .Annotation("Npgsql:PostgresExtension:vault.supabase_vault", ",,");
 
         migrationBuilder.CreateTable(
             name: "issueTypes",
